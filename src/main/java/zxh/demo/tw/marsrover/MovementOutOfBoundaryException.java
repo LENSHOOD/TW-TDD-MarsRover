ca4aedc51c8e001bceaf2e48d902ea6bae1ff5f6
zhangxuhai <xuhai.zhang@thoughtworks.com>
2020-01-14 21:27:48 +0800

    chore: second time

--- src/main/java/zxh/demo/tw/marsrover/MovementOutOfBoundaryException.java
+++
@@ -1,9 +0,0 @@
-package zxh.demo.tw.marsrover;
-
-/**
- * OperationOutOfBoundaryException:
- * @author zhangxuhai
- * @date 2020/1/13
-*/
-public class MovementOutOfBoundaryException extends RuntimeException {
-}
